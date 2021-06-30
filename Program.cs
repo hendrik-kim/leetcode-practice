4819663ac9ef7d7f759553b9116533e30b25bacb
Hendrik Kim <48870515+hendrik-kim@users.noreply.github.com>
2021-06-29 23:15:17 -0500

    init commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace leetcode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ListNode head = new ListNode(0, null);
+            ListNode node1 = new ListNode(1, null);
+            ListNode node2 = new ListNode(2, null);
+            ListNode node3 = new ListNode(3, null);
+
+            head.next = node1;
+            node1.next = node2;
+            node3.next = node3;
+
+            ReverseLinkedList reverse = new ReverseLinkedList();
+
+            reverse.ReverseList(head);
+
+        }
+    }
+}
